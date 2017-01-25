6657375952332555c12a05e52e3d22c3667ace15
FilipeDominguesGit <filipe.domingues.git@gmail.com>
2017-01-25 17:32:33 +0000

    test

--- src/app.js
+++ src/app.js
@@ -39,15 +39,18 @@ bot.dialog('/', function (session) {
                 new builder.HeroCard(session)
                     .title('Hero Card')
                     .subtitle('Kappa.')
-                    .text(`Username: ${session.message.address.user.name}\n 
-                    ChannelId: ${session.message.address.channelId}\n 
-                    Conversation Id: ${session.message.address.conversation.id}`)
+                    .text('text here')
                     .images([
                         builder.CardImage.create(session, 'https://i.ytimg.com/vi/8kBurd4ce0A/maxresdefault.jpg')
                     ])
                     .tap(builder.CardAction.openUrl(session, 'https://i.ytimg.com/vi/8kBurd4ce0A/maxresdefault.jpg'))
             ]);
     session.send(msg);
+    session.send(`Username: ${session.message.address.user.name}\n 
+    User Id: ${session.message.address.user.id}\n 
+                    ChannelId: ${session.message.address.channelId}\n 
+                     Address id: ${session.message.address.id}\n 
+                    Conversation Id: ${session.message.address.conversation.id}`);
 });
 
 
