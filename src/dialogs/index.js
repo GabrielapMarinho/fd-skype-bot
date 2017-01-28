f585f07aced8ae7b0aafc747bbb680caff40a149
FilipeDominguesGit <Filipe.Domingues.Git@gmail.com>
2017-01-28 22:45:26 +0000

    minor text changes

--- src/dialogs/index.js
+++ src/dialogs/index.js
@@ -2,5 +2,5 @@
 module.exports.photoDialog = require('./photo/photoDialog');
 
 module.exports.default = (session)=>{
-    session.endDialog('Welcome! Ask a photo! Just type `Photo`');
+    session.endDialog('Hello! To request a photo, just type `photo` or `photo category`.');
 };
