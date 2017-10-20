bfb4ce8a7e97e1cfd0cdd88cc92158c383872dfa
Filipe Domingues <Filipe.Domingues.Git@gmail.com>
2017-10-20 10:14:00 +0100

    added a test description

--- test/quotesServiceTest.js
+++ test/quotesServiceTest.js
@@ -3,16 +3,17 @@ const assert = require('chai').assert;
 
 const QuotesService =require('../src/services/quotes');
 
-
-it('Should load quotes data from file',(done)=>{
+describe('#Quotes Serice Test.',()=>{
+  it('Should load quotes data from file',(done)=>{
   
-  const quotesService= new QuotesService();
-  quotesService.getRandomQuote()
-    .then((quote)=>{
-      assert.isDefined(quote);
-      assert.isObject(quote);
-      done();
-    })
-    .catch((err)=>done(err)); 
+    const quotesService= new QuotesService();
+    quotesService.getRandomQuote()
+      .then((quote)=>{
+        assert.isDefined(quote);
+        assert.isObject(quote);
+        done();
+      })
+      .catch((err)=>done(err)); 
 
+  });
 });
